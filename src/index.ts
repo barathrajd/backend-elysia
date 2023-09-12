ab657b0c67e3dca26f8669b2bbde0b2edbcf863c
barathrajd <barathdeva757@gmail.com>
2023-09-12 21:09:21 +0530

    route using id and wild types

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,18 @@ import { Elysia } from 'elysia';
 
 const app = new Elysia().get('/', () => 'Hello world From Elysia');
 
+// create a new method using app
+// route - barath
+// callback - returs string barath test
+
 app.get('/barath', () => 'barth test');
+
+// NEED to clarify in the docs
+app.route('M-SEARCH', 'get', () => 'test route');
+
+app.get('/barath/:id', ({ params: { id } }) => ({ id }));
+
+app.get('*', () => 'Not Found Ha Ha Ha Ha');
 
 app.listen(3000);
 
